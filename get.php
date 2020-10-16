a4b38cb2b8e503cc99ecfad7a90099f006317c1d
srsuper <aot5310@gmail.com>
2020-10-16 22:22:54 +0700

    Update get.php

--- get.php
+++ get.php
@@ -1,8 +1,8 @@
 <?php
 echo '3333';
 
-$bot = new \LINE\LINEBot(new CurlHTTPClient('XuAPgE5eH13Hbgj7mSSCmqe5wheTgVDhiE805ypPKx1hyHXCXLgshl02rpLCe+rUUVTfBE6SkoXrkRD0c1omm6o8RFZMgCETtwF7nDTKSg3PDQG6OIHE2npC1e3YfWXhvBMcXBwFrF5zE8s9T83cgQdB04t89/1O/w1cDnyilFU='), [
-    'channelSecret' => '42776475fbb385f32ab3ce678e6f396c'
+$bot = new \LINE\LINEBot(new CurlHTTPClient('427hIu86Wz4W+r6K2gvuRjxsF5KFMOx3riQ4SepQz4iTUcXkM/A5DhbLJ6gTFXQyFDyq0K3RPUWbZwSQy998IuzAoVWyy7EJoUdUmCCfT+UPnj7qQ/Ev3tDLsxYlIqS0c+FIhnPAgCGxNlyGdQ91KgdB04t89/1O/w1cDnyilFU='), [
+    'channelSecret' => 'f0db7adc857fd16b1666e4e29843b183'
 ]);
 
 $res = $bot->getProfile('U1610895050e906214bd1b1e707e3d2f2');
